43fca0e631359abdb87f86c4228b8938cd74b080
Gab Richard <g.elodierichard@gmail.com>
2020-03-09 22:43:21 -0400

    implement user authentification on login and user creation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    # must be created with a password and password confirmation fields
+    it "is valid when password and password confirmation match" do
+      user = User.new name: 'Tiny Tim', email: 'tinytim@xmas.ca', password: 'pass123', password_confirmation: 'pass123'
+      expect(user).to be_valid
+    end
+    # password and password conf need to match
+    it "is not valid when password and password confirmation do not match" do
+      user = User.new name: 'Tiny Tim', email: 'tinytim@xmas.ca', password: 'pass123', password_confirmation: 'pass321'
+      expect(user).to_not be_valid
+    end
+    # emails must be unique and not case sensitive
+    it "is valid when emails is unique (not case sensitive)" do
+      user1 = User.new name: 'Tiny Tim', email: 'tinytim@xmas.ca', password: 'pass123', password_confirmation: 'pass123'
+      user2 = User.new name: 'Mother Mary', email: 'TiNyTiM@XMAS.ca', password: 'pass123', password_confirmation: 'pass123'
+      user1.save
+      expect(user2).to_not be_valid
+    end
+    # email, first name, and last name are also required
+    it "is not valid when email is blank" do
+      user = User.new name: 'Tiny Tim', email: '', password: 'pass123', password_confirmation: 'pass321'
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid when name is blank" do
+      user = User.new name: '', email: 'tinytim@xmas.ca', password: 'pass123', password_confirmation: 'pass321'
+      expect(user).to_not be_valid
+    end
+
+    # the password must have a minimum length when a use account is being created
+    it "is not valid when password is under 3 characters" do
+      user = User.new name: 'Tiny Tim', email: 'tinytim@xmas.ca', password: '12', password_confirmation: '12'
+      expect(user).to_not be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    subject {
+      User.create(name: 'Tiny Tim', email: 'tinytim@xmas.ca', password: 'pass123', password_confirmation: 'pass123')
+    }
+
+    # examples for this class method here
+    it "validates a user with given credentials" do
+      authenticated_user = User.authenticate_with_credentials(subject.email, subject.password)
+      expect(authenticated_user).to eq(subject)
+    end
+
+    it "finds the correct user with a differently-cased email" do
+      authenticated_user = User.authenticate_with_credentials(subject.email.upcase, subject.password)
+      expect(authenticated_user).to eq(subject)
+    end
+
+    it "trims whitespace around email" do
+      authenticated_user = User.authenticate_with_credentials("   #{subject.email}   ", subject.password)
+      expect(authenticated_user).to eq(subject)
+    end
+
+    it "returns nil when given invalid credentials" do
+      authenticated_user = User.authenticate_with_credentials('harry@griffindor.com', 'hermione4ever')
+      expect(authenticated_user).to eq(nil)
+    end
+  end
+
 end
