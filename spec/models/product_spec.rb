b90365897eb5cbf11fa41de86431b3943da6bed3
Gab Richard <g.elodierichard@gmail.com>
2020-03-10 17:07:12 -0400

    removed comment

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
-# Each example will needs its own @category created and then 
-# @product initialized with that category
 RSpec.describe Product, type: :model do
   subject {
     Product.new(name: 'the best book', price_cents: 10, quantity: 4, category: Category.new)
