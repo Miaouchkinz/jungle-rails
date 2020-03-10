f2e16e4f4f9155a0c3473df63f1f8456a229386d
Gab Richard <g.elodierichard@gmail.com>
2020-03-09 22:42:15 -0400

    add validations and authentification on login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :name, presence: true
+  validates :password, length: { minimum: 3 }
+  
+  before_save do
+    self.email = email.downcase
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    user = self.find_by_email email.strip.downcase
+    return user if user && user.authenticate(password)
+    
+    nil
+  end
 end
